f8599dee78e23b8f0f95378d001cda106d4a9223
Mike Laursen <mailbox@laursen.org>
2018-11-07 15:42:20 -0800

    Generated project.
    Modified to be set up for iPhone and portrait only.

---
+++ AddItUp/AddItUp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AddItUp
+//
+//  Created by Mike Laursen on 11/7/18.
+//  Copyright © 2018 Appamajigger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AddItUp/AddItUp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AddItUp
+//
+//  Created by Mike Laursen on 11/7/18.
+//  Copyright © 2018 Appamajigger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
